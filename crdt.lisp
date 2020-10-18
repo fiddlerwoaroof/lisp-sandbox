75ddf051499323075e2e622e961000edf6cb4569
Ed L <el-github@elangley.org>
2020-10-18 01:24:17 -0700

    feat: Adjust crdt for distributed operation

--- crdt.lisp
+++ crdt.lisp
@@ -7,11 +7,40 @@
 (defgeneric crdt-query (crdt))
 (defgeneric crdt-compare (less greater))
 (defgeneric crdt-merge (id first second))
+(defgeneric crdt-serialize (crdt))
+(defgeneric crdt-deserialize (class state)
+  (:method ((class symbol) state)
+    (crdt-deserialize (closer-mop:class-prototype (find-class class))
+                      state)))
 
 (fw.lu:defclass+ g-counter ()
-  ((id :reader my-id :initarg :id)
+  ((lock :reader unsafe-lock
+         :initform (bt:make-lock "g-counter-lock"))
+   (id :reader my-id :initarg :id)
    (size :reader size :initarg :size)
    (payload :reader payload)))
+(defmethod crdt-serialize ((crdt g-counter))
+  (with-output-to-string (s)
+    (format s "~d" (my-id crdt))
+    (loop for v across (bt:with-lock-held ((unsafe-lock crdt))
+                         (copy-seq (payload crdt)))
+          do
+             (format s "|~a" v)
+          finally
+             (format s "~%"))))
+#+comment
+(progn
+  (crdt-serialize (positives *x*))
+  )
+(defmethod crdt-deserialize ((crdt g-counter) state)
+  (let* ((parts (map 'vector 'parse-integer
+                     (fwoar.string-utils:split #\|
+                                               (serapeum:trim-whitespace state))))
+         (result (g-counter (elt parts 0)
+                            (1- (length parts)))))
+    (prog1 result
+      (with-slots (payload) result
+        (setf payload (subseq parts 1))))))
 (defmethod print-object ((o g-counter) s)
   (with-slots (id size) o
     (prin1 `(g-counter :id ,id :size ,size)
@@ -20,8 +49,9 @@
   (with-slots (payload) crdt
     (setf payload (make-array size))))
 (defmethod crdt-update ((op (eql :increment)) (crdt g-counter))
-  (incf (aref (payload crdt)
-              (my-id crdt))))
+  (bt:with-lock-held ((unsafe-lock crdt))
+    (incf (aref (payload crdt)
+                (my-id crdt)))))
 (defmethod crdt-query ((crdt g-counter))
   (reduce #'+
           (payload crdt)))
@@ -38,11 +68,13 @@
           (x y))
   (let ((z (g-counter id (size x))))
     (prog1 z
-      (loop for i from 0 below (size x)
-            do
-               (setf (aref (payload z) i)
-                     (max (aref (payload x) i)
-                          (aref (payload y) i)))))))
+      (bt:with-lock-held ((unsafe-lock x))
+        (bt:with-lock-held ((unsafe-lock y))
+          (loop for i from 0 below (size x)
+                do
+                   (setf (aref (payload z) i)
+                         (max (aref (payload x) i)
+                              (aref (payload y) i)))))))))
 
 (fw.lu:defclass+ pn-counter ()
   ((id :reader my-id :initarg :id)
