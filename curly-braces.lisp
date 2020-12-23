4c7d5ddf3e977a5cb98a8399ea2feba32b19c170
Edward <Langley>
2020-12-23 12:06:46 -0800

    feat(curly-reader): add function macro

--- curly-braces.lisp
+++ curly-braces.lisp
@@ -1,13 +1,22 @@
 (defpackage :curly-bracens
-  (:use :cl :named-readtables))
+  (:use :cl :named-readtables)
+  (:shadow :if :function :return))
 (in-package :curly-bracens)
-(shadow 'if)
 
 (defmacro if (condition then &optional (else-sym nil e-s-p) (else nil e-p))
   (assert (and e-s-p e-p (eq else-sym 'else)))
   `(cl:if ,condition
           ,then
           ,else))
+
+(defmacro function (&body info)
+  (cl:if (symbolp (car info))
+      (destructuring-bind (name args &rest body) info
+         `(defun ,name ,args
+            ,@body))
+      (destructuring-bind (args &rest body) info
+        `(lambda ,args
+           ,@body))))
 
 (defun read-progn (stream char)
   (declare (ignore char))
@@ -18,6 +27,26 @@
   (:merge :standard)
   (:macro-char #\} (lambda (&rest r) (declare (ignore r))) nil)
   (:macro-char #\{ 'read-progn nil))
+
+(defvar return 'return)
+
+
+
+
+
+
+(function do_thing () {
+  return 1;
+})
+
+(function () {
+  return 1;
+})
+
+
+
+
+
 
 (if (> 3 1) {
   (princ :hi)
