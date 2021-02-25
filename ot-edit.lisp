d71eb841d611546fb938c774a9990d6160d4fc5f
Edward <Langley>
2021-02-25 03:22:19 -0800

    refactor: move replace-char, add print-object method

--- ot-edit.lisp
+++ ot-edit.lisp
@@ -12,16 +12,6 @@
 (defgeneric apply-edits (base operations))
 
 
-(fw.lu:defclass+ replace-char ()
-  ((%point :initarg :point :accessor point)
-   (%value :initarg :value :reader value)))
-(defmethod transform-for-op ((op replace-char))
-  (let ((point (point op))
-        (insert-length (length (value op))))
-    (lambda (new-point)
-      (if (<= new-point point)
-          new-point
-          (+ new-point -1 insert-length)))))
 
 (fw.lu:defclass+ insert ()
   ((%point :initarg :point :accessor point)
@@ -56,6 +46,20 @@
           (string-to-edit base) (do-insert string-to-edit (value operation) (point operation))
           (transform base) (alexandria:compose (transform-for-op operation) current-transform))
     base))
+
+(fw.lu:defclass+ replace-char (op)
+  ((%point :initarg :point :accessor point)
+   (%value :initarg :value :reader value)))
+(defmethod print-object ((o replace-char) s)
+  (print-unreadable-object (o s :type t :identity t)
+    (format s "~d/~d" (point o) (epoch o))))
+(defmethod transform-for-op ((op replace-char))
+  (let ((point (point op))
+        (insert-length (length (value op))))
+    (lambda (new-point)
+      (if (<= new-point point)
+          new-point
+          (+ new-point -1 insert-length)))))
 (defun do-replace-char (base new point)
   (let ((begin (subseq base 0 point))
         (end (subseq base (1+ point))))
